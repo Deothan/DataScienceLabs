0c04a378253242bd5757b8ee8fa2974851bd421d
mrr <mads@deothan.dk>
2016-09-24 14:45:33 +0200

    not plotting

--- Lab_2/Data.py
+++
@@ -1,9 +0,0 @@
-class Data:
-
-    def __init__(self, time, gender, height):
-        self.time = time
-        self.gender = gender
-        self.height = height
-
-    def __str__(self):
-        return self.time + " " + self.gender + " " + self.height
---
+++ Lab_3/Iris.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import stats as st
+
+#Loads the CSV file and changes the names of the rows
+iris = pd.read_csv('iris.csv', index_col = False)
+iris.columns = ['Id', 'SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+
+#Make DataFrames for each species
+setosa = iris.loc[iris['Species'] == 'Iris-setosa'].copy()
+plt.subplot("311")
+plt.hist(setosa.iloc[:,1], 50)
+print setosa.iloc[:,1]
+
+versicolor = iris.loc[iris['Species'] == 'Iris-versicolor'].copy()
+virginica = iris.loc[iris['Species'] == 'Iris-virginica'].copy()
+
+#Change the Species to 0, 1 and 2
+iris['Species'] = iris['Species'].map({'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2})
