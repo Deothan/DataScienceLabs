175d6c20d02c42205754c43cda5731a28146315d
mrr <mads@deothan.dk>
2016-09-24 14:47:43 +0200

    new plots

--- Lab_3/Iris.py
+++ Lab_3/Iris.py
@@ -9,12 +9,18 @@ iris.columns = ['Id', 'SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 
 
 #Make DataFrames for each species
 setosa = iris.loc[iris['Species'] == 'Iris-setosa'].copy()
-plt.subplot("311")
+plt.subplot(311)
 plt.hist(setosa.iloc[:,1], 50)
-print setosa.iloc[:,1]
 
 versicolor = iris.loc[iris['Species'] == 'Iris-versicolor'].copy()
+plt.subplot(312)
+plt.hist(versicolor.iloc[:,1], 50)
+
 virginica = iris.loc[iris['Species'] == 'Iris-virginica'].copy()
+plt.subplot(313)
+plt.hist(virginica.iloc[:,1], 50)
 
 #Change the Species to 0, 1 and 2
 iris['Species'] = iris['Species'].map({'Iris-setosa': 0, 'Iris-versicolor': 1, 'Iris-virginica': 2})
+
+plt.show()
